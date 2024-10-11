23e6285756d98ff6a992c3cba23e28e77651b419
guille1006 <guilleia1006@gmail.com>
2024-10-11 21:11:37 +0200

    Dia 5.1

--- prueba.py
+++ prueba.py
@@ -1,2 +1,15 @@
 
-print(int(round(1/2+0.5, 0)))
+def flippingBits(n):
+    binario=bin(n)[2:]
+    longitud=len(binario)
+    binario2=""
+    for i in range(len(binario)):
+        if binario[i]=="1":
+            binario2+="0"
+        elif binario[i]=="0":
+            binario2+="1"
+    binario2="1"*(32-longitud)+binario2
+    decimal=int(binario2,2)
+    print(decimal)
+
+flippingBits(9)
