edc91bae8fe396f25f9643098401f62ff2064cd1
guille1006 <guilleia1006@gmail.com>
2024-10-11 02:46:51 +0200

    Dia 4.1

--- prueba.py
+++ prueba.py
@@ -1,8 +1,3 @@
-def divisibleSumPairs(n, k, ar):
-    if n==2:
-        if sum(ar)%k ==0:
-            return 1
-        else:
-            return 0
 
-print(divisibleSumPairs(2,3,[1,2]))
+lista=[0,1,2,3,4,5,6,7,8]
+lista=lista[1,2,7]
